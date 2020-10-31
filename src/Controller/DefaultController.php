727feff075aec8df1c9db2d1d9c337fe75cac976
Lutty76 <kevin_vezier@yahoo.fr>
2020-10-31 18:20:43 +0100

    place element on pdf page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -41,7 +41,9 @@ class DefaultController extends AbstractController
         
 
           /*return new Response($this->renderView('attestation_page1.svg.twig',
-            [
+            ));*/
+          
+            $data=[
                 'creation_date' => "2020-01-31",
                 'creation_heures' => "15",
                 'creation_minutes' => "20",
@@ -54,19 +56,17 @@ class DefaultController extends AbstractController
                 'fait_heures' => "15",
                 'fait_minutes' => "22",
                 'fait_lieu' => "Mon ul",
-                'motif_travail' => "0",
-                'motif_sante' => "0",
-                'motif_famille' => "0",
-                'motif_handicap' => "0",
-                'motif_sport' => "0",
-                'motif_judiciaire' => "0",
-                'motif_missions' => "0",
-                'motif_enfants' => "0",
+                'motif_travail' => "1",
+                'motif_sante' => "1",
+                'motif_famille' => "1",
+                'motif_handicap' => "1",
+                'motif_sport' => "1",
+                'motif_judiciaire' => "1",
+                'motif_missions' => "1",
+                'motif_enfants' => "1",
                 'motif_courses' => "1",
                 'qrcode' => $qrcodeGenerator->generateQrCode()->writeDataUri()
-            ]));*/
-          
-
+            ];
 
            // $mpdf->WriteHTML($this->renderView('attestation_page2.svg.twig',['qrcode' => ]));
 
@@ -86,8 +86,29 @@ class DefaultController extends AbstractController
             $tplId = $pdf->importPage(1);
             // use the imported page and place it at point 10,10 with a width of 100 mm
             $pdf->useTemplate($tplId, 5, 5, 200);
+            $pdf->SetFont('Arial','',12);
+            $pdf->Text(45, 54.5, $data["prenom"]." ".$data["nom"]);
+            $pdf->Text(45, 61.5, $data["naissance_date"]);
+            $pdf->Text(105, 61.5, $data["naissance_lieu"]);
+            $pdf->Text(49, 69.2, $data["adresse"]);
+
+            if ($data["motif_travail"] == "1")   {   $pdf->Text(31.4, 93.9, 'X');   }
+            if ($data["motif_courses"] == "1")   {   $pdf->Text(31.4, 109, 'X');   }
+            if ($data["motif_sante"] == "1")     {   $pdf->Text(31.4, 127.9, 'X');   }
+            if ($data["motif_famille"] == "1")   {   $pdf->Text(31.4, 141.5, 'X');   }
+            if ($data["motif_handicap"] == "1")  {   $pdf->Text(31.4, 155.3, 'X');   }
+            if ($data["motif_sport"] == "1")     {   $pdf->Text(31.4, 168.2, 'X');   }
+            if ($data["motif_judiciaire"] == "1"){   $pdf->Text(31.4, 189.5, 'X');   }
+            if ($data["motif_missions"] == "1")  {   $pdf->Text(31.4, 202.5, 'X');   }
+            if ($data["motif_enfants"] == "1")   {   $pdf->Text(31.4, 217.8, 'X');   }
+
+            $pdf->Text(41, 229.7, $data["fait_lieu"]);
+            $pdf->Text(36, 237, $data["fait_date"]);
+            $pdf->Text(92, 237, $data["fait_heures"].":".$data["fait_minutes"]);
+
+            $pdf->Image($pic, 150, 220, 35, 35, 'png');
             $pdf->AddPage();
-            $pdf->Image($pic, 10, 30, 100, 100, 'png');
+            $pdf->Image($pic, 10, 10, 100, 100, 'png');
             $pdf->Output();
 
     }
